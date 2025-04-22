79a42b7d85b197b413dc1671a8a3865930df9cfd
linkdom <donglinkang2021@163.com>
2025-04-22 22:15:07 +0800

    modify to use get_pe; F.scaled_dot_product_attention

--- src/models/gpt_v3.py
+++ src/models/gpt_v3.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from .utils import get_pe
 import math
 
 __all__ = ['GPTLanguageModel_v3']
@@ -12,8 +13,12 @@ class NewGELU(nn.Module):
     Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT).
     Reference: Gaussian Error Linear Units (GELU) paper: https://arxiv.org/abs/1606.08415
     """
+    def __init__(self):
+        super().__init__()
+        self.sqrt_2_over_pi = math.sqrt(2.0 / math.pi)
+
     def forward(self, x):
-        return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
+        return 0.5 * x * (1.0 + torch.tanh(self.sqrt_2_over_pi * (x + 0.044715 * torch.pow(x, 3.0))))
 
 class CausalSelfAttention(nn.Module):
     """
@@ -22,46 +27,34 @@ class CausalSelfAttention(nn.Module):
     explicit implementation here to show that there is nothing too scary here.
     """
 
-    def __init__(self, n_embd:int, n_head:int, block_size:int):
+    def __init__(self, n_embd:int, n_head:int):
         super().__init__()
         assert n_embd % n_head == 0
         # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(n_embd, 3 * n_embd)
-        # output projection
-        self.c_proj = nn.Linear(n_embd, n_embd)
-        # causal mask to ensure that attention is only applied to the left in the input sequence
-        self.register_buffer("bias", torch.tril(torch.ones(block_size, block_size))
-                                     .view(1, 1, block_size, block_size))
+        self.c_proj = nn.Linear(n_embd, n_embd) # output projection
         self.n_head = n_head
         self.n_embd = n_embd
 
     def forward(self, x):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
         q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2)
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-
-        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = F.scaled_dot_product_attention(query=q, key=k, value=v, is_causal=True)
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
-
-        # output projection
-        y = self.c_proj(y)
+        y = self.c_proj(y) # output projection
         return y
 
 class Block(nn.Module):
     """ an unassuming Transformer block """
 
-    def __init__(self, n_embd:int, n_head:int, block_size:int):
+    def __init__(self, n_embd:int, n_head:int):
         super().__init__()
         self.ln_1 = nn.LayerNorm(n_embd)
-        self.attn = CausalSelfAttention(n_embd, n_head, block_size)
+        self.attn = CausalSelfAttention(n_embd, n_head)
         self.ln_2 = nn.LayerNorm(n_embd)
         self.mlp = nn.ModuleDict(dict(
             c_fc    = nn.Linear(n_embd, 4 * n_embd),
@@ -79,44 +72,32 @@ class Block(nn.Module):
 class GPTLanguageModel_v3(nn.Module):
     """ Transformer Language Model, exactly as seen in GPT-2 """
 
-    def __init__(self, n_layer:int, n_head:int, n_embd:int, block_size:int, vocab_size:int):
+    def __init__(self, n_layer:int, n_head:int, n_embd:int, block_size:int, vocab_size:int, pe_type:str='randn'):
         super().__init__()
         self.block_size = block_size
 
         self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(vocab_size, n_embd),
-            wpe = nn.Embedding(block_size, n_embd),
-            h = nn.ModuleList([Block(n_embd, n_head, block_size) for _ in range(n_layer)]),
+            wpe = get_pe(pe_type, n_embd, block_size),
+            h = nn.ModuleList([Block(n_embd, n_head) for _ in range(n_layer)]),
             ln_f = nn.LayerNorm(n_embd),
         ))
         self.lm_head = nn.Linear(n_embd, vocab_size, bias=False)
 
-        # report number of parameters (note we don't count the decoder parameters in lm_head)
-        n_params = sum(p.numel() for p in self.transformer.parameters())
-        print("number of parameters: %.2fM" % (n_params/1e6,))
-
-    def get_block_size(self):
-        return self.block_size
-
     def forward(self, idx, targets=None):
-        device = idx.device
-        b, t = idx.size()
-        assert t <= self.block_size, f"Cannot forward sequence of length {t}, block size is only {self.block_size}"
-        pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(0) # shape (1, t)
-
         # forward the GPT model itself
-        tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd)
-        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (1, t, n_embd)
-        x = tok_emb + pos_emb
+        x = self.transformer.wpe(self.transformer.wte(idx)) # (B,T,C)
         for block in self.transformer.h:
             x = block(x)
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x)
 
-        # if we are given some desired targets also calculate the loss
-        loss = None
-        if targets is not None:
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
 
         return logits, loss
-
