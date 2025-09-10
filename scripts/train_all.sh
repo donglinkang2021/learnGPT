1c6c4a2bb117ca59a98c4ff3653880c6f3614478
linkdom <donglinkang2021@163.com>
2025-09-10 07:27:58 +0000

    [exp]train and analyze gpt_v3 again

--- scripts/train_all.sh
+++ scripts/train_all.sh
@@ -1,3 +1,9 @@
 uv run train.py --multirun \
     model=bigram,bow,mlp,rnn,gpt,bigram_v2,bow_v2,gpt_v2,gpt_v3
 
+uv run train.py --multirun \
+    model=gpt_v3 \
+    model.pe_type=randn,sinpe \
+    model.n_embd=384,768 \
+    model.n_head=6,8,12 \
+    model.n_layer=6,8,12
