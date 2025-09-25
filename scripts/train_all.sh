82e704a8e07009f27bf22b46d0dfa74f94f4402a
linkdom <donglinkang2021@163.com>
2025-09-25 08:37:58 +0000

    [exp]train all the exp again

--- scripts/train_all.sh
+++ scripts/train_all.sh
@@ -1,9 +1,9 @@
 uv run train.py --multirun \
-    model=bigram,bow,mlp,rnn,gpt,bigram_v2,bow_v2,gpt_v2,gpt_v3
+    logger=wandb \
+    model=bigram,bow,mlp,rnn,gpt,bigram_v2,bow_v2,gpt_v2,gpt_v3,gpt_rope \
+    training.block_size=256,512,1024
 
 uv run train.py --multirun \
-    model=gpt_v3 \
-    model.pe_type=randn,sinpe \
-    model.n_embd=384,768 \
-    model.n_head=6,8,12 \
-    model.n_layer=6,8,12
+    logger=wandb \
+    model=gpt_attn-mha,gpt_attn-gqa,gpt_attn-mqa,gpt_attn-cla \
+    training.block_size=256,512,1024
